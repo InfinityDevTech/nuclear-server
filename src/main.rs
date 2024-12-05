96c3ad023bb04946ff14f4e284b2741ffb6364d1
InfinityDevTech <infinity@infinity-dev.xyz>
2024-12-05 11:35:08 -0500

    Added basic session searching

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,46 @@
 use std::env;
 
-use dotenv::dotenv;
 use db::{DBCollections, Perks, UserData, COLLECTIONS, DATABASE_NAME, DB};
+use dotenv::dotenv;
 use log::info;
 use mongodb::bson::doc;
 use rocket::{http::Status, serde::json::Json};
 use rocket_db_pools::{mongodb::Collection, Connection, Database};
 
-#[macro_use] extern crate rocket;
-pub mod logging;
+#[macro_use]
+extern crate rocket;
 pub mod db;
+pub mod logging;
+pub mod session;
 
 #[post("/login/<id>")]
 async fn post_register(id: &str, db: Connection<DB>) -> Result<Json<UserData>, Status> {
     // Get the steam user data, this is to check if the user exists
     // and get the users username and stuffs.
-    let collection: Collection<UserData> = db.database(DATABASE_NAME).collection(&COLLECTIONS[DBCollections::UserStatCollection]);
+    let collection: Collection<UserData> = db
+        .database(DATABASE_NAME)
+        .collection(&COLLECTIONS[DBCollections::UserStatCollection]);
 
-    let potential_user = collection.find_one(
-        doc! {
-            "steam_id": id
-        },
-        None
-    ).await;
+    let potential_user = collection
+        .find_one(
+            doc! {
+                "steam_id": id
+            },
+            None,
+        )
+        .await;
 
     if let Ok(Some(potential_user)) = potential_user {
         return Ok(Json(potential_user));
     }
 
-    if let Ok(res) = reqwest::get(format!("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}", env::var("STEAM_API_KEY").unwrap(), id)).await {
+    if let Ok(res) = reqwest::get(format!(
+        "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}",
+        env::var("STEAM_API_KEY").unwrap(),
+        id
+    ))
+    .await
+    {
         let data = res.json::<serde_json::Value>().await;
 
         if data.is_err() {
@@ -36,15 +48,23 @@ async fn post_register(id: &str, db: Connection<DB>) -> Result<Json<UserData>, S
         }
         let data = data.unwrap();
 
-        if data["response"]["players"].is_array() && data["response"]["players"].as_array().unwrap().is_empty() {
+        if data["response"]["players"].is_array()
+            && data["response"]["players"].as_array().unwrap().is_empty()
+        {
             return Err(Status::NoContent);
         }
 
         let user_data = UserData {
-            steam_id: data["response"]["players"][0]["steamid"].as_str().unwrap().to_string(),
-            last_known_username: data["response"]["players"][0]["personaname"].as_str().unwrap().to_string(),
+            steam_id: data["response"]["players"][0]["steamid"]
+                .as_str()
+                .unwrap()
+                .to_string(),
+            last_known_username: data["response"]["players"][0]["personaname"]
+                .as_str()
+                .unwrap()
+                .to_string(),
             points: 0,
-            perks: Perks::default()
+            perks: Perks::default(),
         };
 
         let result = collection.insert_one(user_data.clone(), None).await;
@@ -61,13 +81,17 @@ async fn post_register(id: &str, db: Connection<DB>) -> Result<Json<UserData>, S
 
 #[get("/data/<id>")]
 async fn get_data(id: &str, db: Connection<DB>) -> Result<Json<UserData>, Status> {
-    let collection: Collection<UserData> = db.database(DATABASE_NAME).collection(&COLLECTIONS[DBCollections::UserStatCollection]);
-    let result = collection.find_one(
-        doc! {
-            "steam_id": id
-        },
-        None
-    ).await;
+    let collection: Collection<UserData> = db
+        .database(DATABASE_NAME)
+        .collection(&COLLECTIONS[DBCollections::UserStatCollection]);
+    let result = collection
+        .find_one(
+            doc! {
+                "steam_id": id
+            },
+            None,
+        )
+        .await;
 
     if let Ok(result) = result {
         if result.is_none() {
@@ -82,14 +106,18 @@ async fn get_data(id: &str, db: Connection<DB>) -> Result<Json<UserData>, Status
 
 #[post("/data/<id>/update_points/<points>")]
 async fn update_points(id: &str, points: i32, db: Connection<DB>) -> Status {
-    let collection: Collection<UserData> = db.database(DATABASE_NAME).collection(&COLLECTIONS[DBCollections::UserStatCollection]);
+    let collection: Collection<UserData> = db
+        .database(DATABASE_NAME)
+        .collection(&COLLECTIONS[DBCollections::UserStatCollection]);
 
-    let user = collection.find_one(
-        doc! {
-            "steam_id": id
-        },
-        None
-    ).await;
+    let user = collection
+        .find_one(
+            doc! {
+                "steam_id": id
+            },
+            None,
+        )
+        .await;
 
     if let Ok(Some(mut user)) = user {
         user.points += points as i64;
@@ -98,17 +126,19 @@ async fn update_points(id: &str, points: i32, db: Connection<DB>) -> Status {
             user.points = 0;
         }
 
-        let result = collection.update_one(
-            doc! {
-                "steam_id": id
-            },
-            doc! {
-                "$set": {
-                    "points": user.points
-                }
-            },
-            None
-        ).await;
+        let result = collection
+            .update_one(
+                doc! {
+                    "steam_id": id
+                },
+                doc! {
+                    "$set": {
+                        "points": user.points
+                    }
+                },
+                None,
+            )
+            .await;
 
         if let Ok(_result) = result {
             Status::Ok
@@ -134,17 +164,23 @@ async fn rocket() -> _ {
     logging::init_logger();
     logging::clean_old();
 
-    let figment = rocket::Config::figment()
-        .merge(("databases.nuclear_db", rocket_db_pools::Config {
+    let figment = rocket::Config::figment().merge((
+        "databases.nuclear_db",
+        rocket_db_pools::Config {
             url: env::var("MONGO_DB_URL").unwrap(),
             min_connections: None,
             max_connections: 1024,
             connect_timeout: 3,
             idle_timeout: None,
             extensions: None,
-        }));
+        },
+    ));
 
-        info!("Starting Rocket server...");
-    rocket::custom(figment).attach(DB::init()).mount("/", routes![index, get_data, update_points, post_register])
+    session::setup_hashmap();
+    
+    info!("Starting Rocket server...");
+    rocket::custom(figment)
+        .attach(DB::init())
+        .mount("/", routes![index, get_data, update_points, post_register, session::get_sessions_by_region, session::list_regions])
     //rocket::build().attach(DB::init()).mount("/", routes![index, get_points, post_register])
 }
---
+++ src/session.rs
@@ -0,0 +1,129 @@
+use std::{
+    collections::HashMap,
+    sync::Mutex,
+};
+
+use lazy_static::lazy_static;
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug, Serialize, Deserialize, Clone, Hash, PartialEq, PartialOrd)]
+pub enum GameDifficulty {
+    Easy,
+    Normal,
+    Hard,
+    Nightmare,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Hash, PartialEq, Eq, PartialOrd, EnumIter)]
+pub enum Region {
+    NorthAmerica,
+    SouthAmerica,
+    Asia,
+    Europe,
+    Africa,
+    Australia,
+    Antarctica,
+}
+
+pub fn region_from_string(region: String) -> Region {
+    let string = region.to_lowercase();
+
+    match string.as_str() {
+        "northamerica" => Region::NorthAmerica,
+        "southamerica" => Region::SouthAmerica,
+        "asia" => Region::Asia,
+        "europe" => Region::Europe,
+        "africa" => Region::Africa,
+        "australia" => Region::Australia,
+        "antarctica" => Region::Antarctica,
+        _ => Region::NorthAmerica,
+    }
+}
+
+pub fn string_from_region(region: Region) -> String {
+    match region {
+        Region::NorthAmerica => "NorthAmerica".to_string(),
+        Region::SouthAmerica => "SouthAmerica".to_string(),
+        Region::Asia => "Asia".to_string(),
+        Region::Europe => "Europe".to_string(),
+        Region::Africa => "Africa".to_string(),
+        Region::Australia => "Australia".to_string(),
+        Region::Antarctica => "Antarctica".to_string(),
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Session {
+    pub session_id: String,
+    pub region: Region,
+    pub host_steam_id: String,
+    pub players: Vec<String>,
+    pub difficulty: GameDifficulty,
+    pub started: bool,
+    pub extraction_enabled: bool,
+    pub friendly_fire: bool,
+}
+
+lazy_static! {
+    pub static ref SESSIONS: Mutex<HashMap<String, Session>> = Mutex::new(HashMap::new());
+    pub static ref SESSIONS_BY_REGION: Mutex<HashMap<Region, Vec<String>>> =
+        Mutex::new(HashMap::new());
+}
+
+pub fn setup_hashmap() {
+    let mut regions = SESSIONS_BY_REGION.lock().unwrap();
+
+    for region in Region::iter() {
+        regions.insert(region.clone(), vec![]);
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RegionListRes {
+    pub regions: Vec<String>,
+}
+
+#[get("/sessions/regions")]
+pub async fn list_regions() -> Json<RegionListRes> {
+    let regions = SESSIONS_BY_REGION.lock().unwrap();
+    let mut regions_list = Vec::new();
+
+    for region in regions.keys() {
+        regions_list.push(string_from_region(region.clone()));
+    }
+
+    Json(RegionListRes {
+        regions: regions_list,
+    })
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SessionListRes {
+    pub region: String,
+    pub sessions: Vec<Session>,
+}
+
+#[get("/sessions/list/<region>")]
+pub async fn get_sessions_by_region(region: &str) -> Json<SessionListRes> {
+    let sessions = SESSIONS.lock().unwrap();
+    let sessions_by_region = SESSIONS_BY_REGION.lock().unwrap();
+    let region = region_from_string(region.to_string());
+
+    let session_ids = sessions_by_region.get(&region).unwrap();
+    let mut sessions_by_region = Vec::new();
+
+    for session_id in session_ids {
+        let session = sessions.get(session_id).unwrap();
+        sessions_by_region.push(session.clone());
+    }
+
+    let res = SessionListRes {
+        region: string_from_region(region),
+        sessions: sessions_by_region,
+    };
+
+    Json(res)
+}
